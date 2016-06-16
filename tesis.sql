5b48f95da64444acee86ffb72f7cde8098230850
Pablo Lozano <pablolozanogonzalez@gmail.com>
2016-06-16 11:32:44 -0300

    FIXED AND FUNCTIONAL

--- tesis.sql
+++ tesis.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-06-2016 a las 21:38:42
--- Versión del servidor: 5.6.26
--- Versión de PHP: 5.6.12
+-- Tiempo de generación: 16-06-2016 a las 10:34:00
+-- Versión del servidor: 5.5.27
+-- Versión de PHP: 5.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `tesis`
@@ -30,7 +30,8 @@ CREATE TABLE IF NOT EXISTS `login_session` (
   `username` varchar(60) NOT NULL,
   `status` varchar(6) NOT NULL DEFAULT 'OFF',
   `ipAdd` varchar(60) NOT NULL,
-  `log_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `log_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -38,8 +39,10 @@ CREATE TABLE IF NOT EXISTS `login_session` (
 --
 
 INSERT INTO `login_session` (`username`, `status`, `ipAdd`, `log_time`) VALUES
-('admin', 'OFF', '', '2016-06-09 02:06:40'),
-('PC2', 'ON', '127.0.0.1', '2016-06-09 02:06:48');
+('admin', 'OFF', '::1', '2016-06-16 14:28:42'),
+('pc1', 'OFF', '::1', '2016-06-16 14:27:26'),
+('pc2', 'OFF', '::1', '2016-06-16 13:24:45'),
+('pc3', 'OFF', '::1', '2016-06-16 14:15:40');
 
 -- --------------------------------------------------------
 
@@ -48,12 +51,13 @@ INSERT INTO `login_session` (`username`, `status`, `ipAdd`, `log_time`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `login_status` (
-  `id` mediumint(9) NOT NULL,
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
   `ipAdd` varchar(45) NOT NULL,
   `fecha` datetime NOT NULL,
-  `type` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -64,9 +68,10 @@ CREATE TABLE IF NOT EXISTS `login_status` (
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `normalIP` varchar(45) DEFAULT NULL,
+  `normalIP` varchar(45) NOT NULL,
   `estatus` int(2) NOT NULL,
-  `pass_time` timestamp NULL DEFAULT NULL
+  `pass_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -74,44 +79,13 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 --
 
 INSERT INTO `usuarios` (`username`, `password`, `normalIP`, `estatus`, `pass_time`) VALUES
-('admin', 'root', '127.0.0.1', 0, NULL),
-('PC1', 'pc1234', NULL, 0, NULL),
-('PC2', 'pc1234', NULL, 1, '2016-06-09 00:04:32'),
-('PC3', 'pc1234', NULL, 0, NULL),
-('PC4', 'pc1234', NULL, 0, NULL),
-('root', 'admin', NULL, 0, NULL);
+('admin', 'root', '::1', 0, '2016-06-16 14:23:25'),
+('PC1', 'pc1234', '::1', 2, '2016-06-16 14:27:25'),
+('PC2', 'pc4321', '::1', 1, '2016-06-16 12:45:07'),
+('PC3', 'pc1234', '::1', 1, '2016-06-16 14:15:31'),
+('PC4', 'pc1234', '', 0, NULL),
+('root', 'admin', '', 0, NULL);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `login_session`
---
-ALTER TABLE `login_session`
-  ADD PRIMARY KEY (`username`);
-
---
--- Indices de la tabla `login_status`
---
-ALTER TABLE `login_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`username`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `login_status`
---
-ALTER TABLE `login_status`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
